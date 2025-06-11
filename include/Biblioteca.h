374c7bc0a2a1769186d9358abc6cb80a37c212aa
yohannd1 <mitonanan12@gmail.com>
2025-06-11 11:43:01 -0300

    iniciando separação de funções + adicionando editorconfig

--- include/Biblioteca.h
+++ include/Biblioteca.h
@@ -10,19 +10,24 @@ extern int **imagemR, **imagemG, **imagemB, ncol, nlin, quantizacao;
 extern char nome_entrada[100];
 extern char nome_base[100];
 
-// Funções
+// Funções de leitura e escrita de imagem
 void ler_cabecalho(void);
 void ler_imagem(void);
 void remover_extensao(char *nome);
 int carregar_imagem(void);
-void liberar_memoria(void);
 void salvar_ppm(const char *nome_arquivo, int **R, int **G, int **B, int largura, int altura);
+
+// Funções gerais
+void liberar_memoria(void);
+void liberar_imagem(int **imagem, int altura);
+
+// Funções matemáticas
 int clamp(int val, int min, int max);
 int media9(int **matriz, int x, int y, int largura, int altura);
+
+// Funções de amplicação & redução
 void ampliacao_vizinho_mais_proximo(int novaLargura, int novaAltura, int ***outR, int ***outG, int ***outB);
 void reducao_vizinho(int novaLargura, int novaAltura, int ***outR, int ***outG, int ***outB);
 void reducao_biquadratica(int novaLargura, int novaAltura, int ***outR, int ***outG, int ***outB);
-void liberar_imagem(int **imagem, int altura);
-
 
 #endif
