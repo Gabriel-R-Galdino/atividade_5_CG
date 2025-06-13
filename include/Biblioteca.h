c9973c16815f7abbabc7d1353e736ffaa5d04887
Gabriel Galdino <galdinobrossx1@gmail.com>
2025-06-13 12:33:11 -0300

    criando branch de teste

--- include/Biblioteca.h
+++ include/Biblioteca.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Variáveis globais
 extern FILE *fpin;
--- src/Main.c
+++ src/Main.c
@@ -8,68 +8,69 @@ int main() {
     ler_imagem();
     fclose(fpin);
 
-    printf("Imagem lida!\n");
-    printf("Dimensões atuais: %dx%d\n", ncol, nlin);
+    int novaLarguraAmpl = ncol * 2;
+    int novaAlturaAmpl = nlin * 2;
+    int novaLarguraReduc = ncol / 2;
+    int novaAlturaReduc = nlin / 2;
 
-    int novaLarguraAmpl, novaAlturaAmpl;
-    printf("Digite a nova largura e altura (para ampliação):");
-    scanf("%d %d", &novaLarguraAmpl, &novaAlturaAmpl);
-    if (novaLarguraAmpl <= ncol || novaAlturaAmpl <= nlin) {
-        printf("Tanto a altura quanto a largura devem ser maiores que o valor atual!\n");
-        return 1;
-    }
-
-    int novaLarguraReduc, novaAlturaReduc;
-    printf("Digite a nova largura e altura (para redução):");
-    scanf("%d %d", &novaLarguraReduc, &novaAlturaReduc);
-    if (novaLarguraReduc >= ncol || novaAlturaReduc >= nlin) {
-        printf("Tanto a altura quanto a largura devem ser menores que o valor atual!\n");
-        return 1;
-    }
-
-    printf("Gerando imagens...\n");
-
-    // Variáveis para receber as imagens processadas
     int **reduzidaR, **reduzidaG, **reduzidaB, **ampliadaR, **ampliadaG, **ampliadaB;
+    char nomeAmplVizinho[150], nomeReducVizinho[150];
+    clock_t inicio, fim;
+    double tempo;
 
-    char nomeAmplVizinho[150];
-    char nomeReducVizinho[150];
-
-    // 1. Ampliação com vizinho mais próximo
+    // 1. Ampliação - Vizinho mais próximo
+    inicio = clock();
     vizinho_proximo(novaLarguraAmpl, novaAlturaAmpl, &ampliadaR, &ampliadaG, &ampliadaB, 1);
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo para ampliação (vizinho mais próximo): %.3f segundos\n\n\n", tempo);
+
     nome_arquivo(nomeAmplVizinho, "_ampliada_vizinho");
     salvar_ppm(nomeAmplVizinho, ampliadaR, ampliadaG, ampliadaB, novaLarguraAmpl, novaAlturaAmpl);
-
     liberar_imagem(ampliadaR, novaAlturaAmpl);
     liberar_imagem(ampliadaG, novaAlturaAmpl);
     liberar_imagem(ampliadaB, novaAlturaAmpl);
 
-    // 2. Ampliação com interpolação linear (4 vizinhos)
+    // 2. Ampliação - Interpolação bilinear
+    inicio = clock();
     interpolacao_bilinear(novaLarguraAmpl, novaAlturaAmpl, &ampliadaR, &ampliadaG, &ampliadaB, 1);
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo para ampliação (interpolação bilinear): %.3f segundos\n\n\n", tempo);
+
     nome_arquivo(nomeAmplVizinho, "_ampliada_bilinear");
     salvar_ppm(nomeAmplVizinho, ampliadaR, ampliadaG, ampliadaB, novaLarguraAmpl, novaAlturaAmpl);
+    liberar_imagem(ampliadaR, novaAlturaAmpl);
+    liberar_imagem(ampliadaG, novaAlturaAmpl);
+    liberar_imagem(ampliadaB, novaAlturaAmpl);
 
-    // 3. Redução por vizinho mais próximo
+    // 3. Redução - Vizinho mais próximo
+    inicio = clock();
     vizinho_proximo(novaLarguraReduc, novaAlturaReduc, &reduzidaR, &reduzidaG, &reduzidaB, 0);
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo para redução (vizinho mais próximo): %.3f segundos\n\n\n", tempo);
+
     nome_arquivo(nomeReducVizinho, "_reduzida_vizinho");
     salvar_ppm(nomeReducVizinho, reduzidaR, reduzidaG, reduzidaB, novaLarguraReduc, novaAlturaReduc);
-
     liberar_imagem(reduzidaR, novaAlturaReduc);
     liberar_imagem(reduzidaG, novaAlturaReduc);
     liberar_imagem(reduzidaB, novaAlturaReduc);
 
-    // 4. Redução com interpolação biquadrática (9 vizinhos)
+    // 4. Redução - Interpolação biquadrática
+    inicio = clock();
     reducao_biquadratica(novaLarguraReduc, novaAlturaReduc, &reduzidaR, &reduzidaG, &reduzidaB);
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo para redução (interpolação biquadrática): %.3f segundos\n\n\n", tempo);
+
     nome_arquivo(nomeReducVizinho, "_reduzida_biquadratica");
     salvar_ppm(nomeReducVizinho, reduzidaR, reduzidaG, reduzidaB, novaLarguraReduc, novaAlturaReduc);
-
     liberar_imagem(reduzidaR, novaAlturaReduc);
     liberar_imagem(reduzidaG, novaAlturaReduc);
     liberar_imagem(reduzidaB, novaAlturaReduc);
 
-    // Liberar a imagem original
     liberar_memoria();
-
     printf("Imagens geradas com sucesso!\n");
 
     return 0;
